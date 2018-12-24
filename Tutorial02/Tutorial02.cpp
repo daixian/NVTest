8397c418b5c2284cb30eec640da5dd2b5a23b6b8
daixian <amano_tooko@qq.com>
2018-12-24 22:03:34 +0800

    窗口模式的版本

--- Tutorial02/Tutorial02.cpp
+++ Tutorial02/Tutorial02.cpp
@@ -19,6 +19,19 @@
 #include <directxcolors.h>
 #include "resource.h"
 
+//#include "nvapi.h"
+//#pragma comment(lib, "nvapi.lib")
+
+
+#define RELEASE_D3D11_1_OBJECT(o)   \
+  if (o != NULL)                    \
+  {                                 \
+    o->Release();                   \
+    o = NULL;                       \
+  }
+
+
+
 using namespace DirectX;
 
 //--------------------------------------------------------------------------------------
@@ -43,7 +56,14 @@ ID3D11DeviceContext*    g_pImmediateContext = nullptr;
 ID3D11DeviceContext1*   g_pImmediateContext1 = nullptr;
 IDXGISwapChain*         g_pSwapChain = nullptr;
 IDXGISwapChain1*        g_pSwapChain1 = nullptr;
-ID3D11RenderTargetView* g_pRenderTargetView = nullptr;
+
+ID3D11RenderTargetView* g_backBufferRenderTargetView = nullptr;
+ID3D11DepthStencilView*   g_backBufferDepthStencilView = NULL;
+ID3D11RenderTargetView*   g_rightRenderTargetView = NULL;
+
+ID3D11Buffer*             g_staticShaderConstantsBuffer = NULL;
+ID3D11Buffer*             g_dynamicShaderConstantsBuffer = NULL;
+
 ID3D11VertexShader*     g_pVertexShader = nullptr;
 ID3D11PixelShader*      g_pPixelShader = nullptr;
 ID3D11InputLayout*      g_pVertexLayout = nullptr;
@@ -59,7 +79,9 @@ void CleanupDevice();
 LRESULT CALLBACK    WndProc( HWND, UINT, WPARAM, LPARAM );
 void Render();
 
-
+void clearScene(int eye);
+void setRenderTarget(int eye);
+bool createBackBufferViews();
 //--------------------------------------------------------------------------------------
 // Entry point to the program. Initializes everything and goes into a message processing 
 // loop. Idle time is used to render the scene.
@@ -182,6 +204,7 @@ HRESULT CompileShaderFromFile( const WCHAR* szFileName, LPCSTR szEntryPoint, LPC
 //--------------------------------------------------------------------------------------
 HRESULT InitDevice()
 {
+
     HRESULT hr = S_OK;
 
     RECT rc;
@@ -205,9 +228,6 @@ HRESULT InitDevice()
     D3D_FEATURE_LEVEL featureLevels[] =
     {
         D3D_FEATURE_LEVEL_11_1,
-        D3D_FEATURE_LEVEL_11_0,
-        D3D_FEATURE_LEVEL_10_1,
-        D3D_FEATURE_LEVEL_10_0,
     };
 	UINT numFeatureLevels = ARRAYSIZE( featureLevels );
 
@@ -269,7 +289,21 @@ HRESULT InitDevice()
         sd.SampleDesc.Count = 1;
         sd.SampleDesc.Quality = 0;
         sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-        sd.BufferCount = 1;
+        sd.BufferCount = 2;
+        sd.Stereo = TRUE;
+        sd.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
+
+        // Set up the full screen swap chain descriptor.
+        DXGI_RATIONAL refreshRate;
+        refreshRate.Numerator = 120;
+        refreshRate.Denominator = 1;
+
+        DXGI_SWAP_CHAIN_FULLSCREEN_DESC swapChainFullScreenDesc;
+        swapChainFullScreenDesc.RefreshRate = refreshRate;
+        swapChainFullScreenDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_PROGRESSIVE;
+        swapChainFullScreenDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+        swapChainFullScreenDesc.Windowed = FALSE; //窗口模式
+
 
         hr = dxgiFactory2->CreateSwapChainForHwnd( g_pd3dDevice, g_hWnd, &sd, nullptr, nullptr, &g_pSwapChain1 );
         if (SUCCEEDED(hr))
@@ -287,7 +321,7 @@ HRESULT InitDevice()
         sd.BufferDesc.Width = width;
         sd.BufferDesc.Height = height;
         sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-        sd.BufferDesc.RefreshRate.Numerator = 60;
+        sd.BufferDesc.RefreshRate.Numerator = 120;
         sd.BufferDesc.RefreshRate.Denominator = 1;
         sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
         sd.OutputWindow = g_hWnd;
@@ -306,18 +340,23 @@ HRESULT InitDevice()
     if (FAILED(hr))
         return hr;
 
-    // Create a render target view
-    ID3D11Texture2D* pBackBuffer = nullptr;
-    hr = g_pSwapChain->GetBuffer( 0, __uuidof( ID3D11Texture2D ), reinterpret_cast<void**>( &pBackBuffer ) );
-    if( FAILED( hr ) )
-        return hr;
+    //// Create a render target view
+    //ID3D11Texture2D* pBackBuffer = nullptr;
+    //hr = g_pSwapChain->GetBuffer( 0, __uuidof( ID3D11Texture2D ), reinterpret_cast<void**>( &pBackBuffer ) );
+    //if( FAILED( hr ) )
+    //    return hr;
 
-    hr = g_pd3dDevice->CreateRenderTargetView( pBackBuffer, nullptr, &g_pRenderTargetView );
-    pBackBuffer->Release();
-    if( FAILED( hr ) )
-        return hr;
+    //hr = g_pd3dDevice->CreateRenderTargetView( pBackBuffer, nullptr, &g_pRenderTargetView );
+    //pBackBuffer->Release();
+    //if( FAILED( hr ) )
+    //    return hr;
 
-    g_pImmediateContext->OMSetRenderTargets( 1, &g_pRenderTargetView, nullptr );
+    //g_pImmediateContext->OMSetRenderTargets( 1, &g_pRenderTargetView, nullptr );
+
+    if (!createBackBufferViews())
+    {
+        return hr;
+    }
 
     // Setup the viewport
     D3D11_VIEWPORT vp;
@@ -330,55 +369,55 @@ HRESULT InitDevice()
     g_pImmediateContext->RSSetViewports( 1, &vp );
 
     // Compile the vertex shader
-    //ID3DBlob* pVSBlob = nullptr;
-    //hr = CompileShaderFromFile( L"Tutorial02.fx", "VS", "vs_4_0", &pVSBlob );
-    //if( FAILED( hr ) )
-    //{
-    //    MessageBox( nullptr,
-    //                L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK );
-    //    return hr;
-    //}
+    ID3DBlob* pVSBlob = nullptr;
+    hr = CompileShaderFromFile( L"Tutorial02.fx", "VS", "vs_4_0", &pVSBlob );
+    if( FAILED( hr ) )
+    {
+        MessageBox( nullptr,
+                    L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK );
+        return hr;
+    }
 
 	// Create the vertex shader
-	//hr = g_pd3dDevice->CreateVertexShader( pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), nullptr, &g_pVertexShader );
-	//if( FAILED( hr ) )
-	//{	
-	//	pVSBlob->Release();
-    //  return hr;
-	//}
+	hr = g_pd3dDevice->CreateVertexShader( pVSBlob->GetBufferPointer(), pVSBlob->GetBufferSize(), nullptr, &g_pVertexShader );
+	if( FAILED( hr ) )
+	{	
+		pVSBlob->Release();
+      return hr;
+	}
 
     // Define the input layout
- //   D3D11_INPUT_ELEMENT_DESC layout[] =
- //   {
- //       { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
- //   };
-	//UINT numElements = ARRAYSIZE( layout );
+    D3D11_INPUT_ELEMENT_DESC layout[] =
+    {
+        { "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+    };
+	UINT numElements = ARRAYSIZE( layout );
 
- //   // Create the input layout
-	//hr = g_pd3dDevice->CreateInputLayout( layout, numElements, pVSBlob->GetBufferPointer(),
- //                                         pVSBlob->GetBufferSize(), &g_pVertexLayout );
-	//pVSBlob->Release();
-	//if( FAILED( hr ) )
- //       return hr;
+    // Create the input layout
+	hr = g_pd3dDevice->CreateInputLayout( layout, numElements, pVSBlob->GetBufferPointer(),
+                                          pVSBlob->GetBufferSize(), &g_pVertexLayout );
+	pVSBlob->Release();
+	if( FAILED( hr ) )
+        return hr;
 
     // Set the input layout
-    //g_pImmediateContext->IASetInputLayout( g_pVertexLayout );
+    g_pImmediateContext->IASetInputLayout( g_pVertexLayout );
 
 	// Compile the pixel shader
-	//ID3DBlob* pPSBlob = nullptr;
- //   hr = CompileShaderFromFile( L"Tutorial02.fx", "PS", "ps_4_0", &pPSBlob );
- //   if( FAILED( hr ) )
- //   {
- //       MessageBox( nullptr,
- //                   L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK );
- //       return hr;
- //   }
+	ID3DBlob* pPSBlob = nullptr;
+    hr = CompileShaderFromFile( L"Tutorial02.fx", "PS", "ps_4_0", &pPSBlob );
+    if( FAILED( hr ) )
+    {
+        MessageBox( nullptr,
+                    L"The FX file cannot be compiled.  Please run this executable from the directory that contains the FX file.", L"Error", MB_OK );
+        return hr;
+    }
 
 	//// Create the pixel shader
-	//hr = g_pd3dDevice->CreatePixelShader( pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), nullptr, &g_pPixelShader );
-	//pPSBlob->Release();
- //   if( FAILED( hr ) )
- //       return hr;
+	hr = g_pd3dDevice->CreatePixelShader( pPSBlob->GetBufferPointer(), pPSBlob->GetBufferSize(), nullptr, &g_pPixelShader );
+	pPSBlob->Release();
+    if( FAILED( hr ) )
+        return hr;
 
     // Create vertex buffer
     SimpleVertex vertices[] =
@@ -422,7 +461,15 @@ void CleanupDevice()
     if( g_pVertexLayout ) g_pVertexLayout->Release();
     if( g_pVertexShader ) g_pVertexShader->Release();
     if( g_pPixelShader ) g_pPixelShader->Release();
-    if( g_pRenderTargetView ) g_pRenderTargetView->Release();
+    //if( g_pRenderTargetView ) g_pRenderTargetView->Release();
+
+    RELEASE_D3D11_1_OBJECT(g_staticShaderConstantsBuffer);
+    RELEASE_D3D11_1_OBJECT(g_dynamicShaderConstantsBuffer);
+
+    RELEASE_D3D11_1_OBJECT(g_backBufferRenderTargetView);
+    RELEASE_D3D11_1_OBJECT(g_backBufferDepthStencilView);
+    RELEASE_D3D11_1_OBJECT(g_rightRenderTargetView);
+
     if( g_pSwapChain1 ) g_pSwapChain1->Release();
     if( g_pSwapChain ) g_pSwapChain->Release();
     if( g_pImmediateContext1 ) g_pImmediateContext1->Release();
@@ -461,20 +508,139 @@ LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam 
     return 0;
 }
 
-
 //--------------------------------------------------------------------------------------
 // Render a frame
 //--------------------------------------------------------------------------------------
 void Render()
 {
+    setRenderTarget(0);
+
     // Clear the back buffer 
-    g_pImmediateContext->ClearRenderTargetView( g_pRenderTargetView, Colors::MidnightBlue );
+    //g_pImmediateContext->ClearRenderTargetView(g_backBufferRenderTargetView, Colors::MidnightBlue );
+    clearScene(0);
 
     // Render a triangle
 	g_pImmediateContext->VSSetShader( g_pVertexShader, nullptr, 0 );
 	g_pImmediateContext->PSSetShader( g_pPixelShader, nullptr, 0 );
     g_pImmediateContext->Draw( 3, 0 );
 
+    setRenderTarget(1);
+
+    clearScene(1);
+
+    g_pImmediateContext->VSSetShader(g_pVertexShader, nullptr, 0);
+    g_pImmediateContext->PSSetShader(g_pPixelShader, nullptr, 0);
+    g_pImmediateContext->Draw(3, 0);
+
     // Present the information rendered to the back buffer to the front buffer (the screen)
-    g_pSwapChain->Present( 0, 0 );
+    g_pSwapChain->Present( 1, 0 );
+}
+
+void setRenderTarget(int eye)
+{
+    switch (eye)
+    {
+    case 0:
+        g_pImmediateContext1->OMSetRenderTargets(1, &g_backBufferRenderTargetView, nullptr); //g_backBufferDepthStencilView 这里如果设置了深度那么就会不存在currentRenderTargetView
+        g_backBufferRenderTargetView->Release();
+        break;
+    case 1:
+        g_pImmediateContext1->OMSetRenderTargets(1, &g_rightRenderTargetView, nullptr);
+        g_rightRenderTargetView->Release();
+        break;
+    default:
+        break;
+    }
+}
+
+
+void clearScene(int eye)
+{
+    ID3D11RenderTargetView* currentRenderTargetView;
+    g_pImmediateContext1->OMGetRenderTargets(1, &currentRenderTargetView, NULL);
+
+    // Clear the back buffer.
+    if (currentRenderTargetView)
+    {
+        if (eye == 0)
+        {
+            float clearColor[4] = { 0.0f, 1.0f, 0.0f, 1.0f }; // red, green, blue, alpha
+            g_pImmediateContext1->ClearRenderTargetView(currentRenderTargetView, clearColor);
+            g_pImmediateContext1->ClearDepthStencilView(g_backBufferDepthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+        }
+        else
+        {
+            float clearColor[4] = { 1.0f, 0.0f, 1.0f, 1.0f }; // red, green, blue, alpha
+            g_pImmediateContext1->ClearRenderTargetView(currentRenderTargetView, clearColor);
+            g_pImmediateContext1->ClearDepthStencilView(g_backBufferDepthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+        }
+
+    }
+}
+
+bool createBackBufferViews()
+{
+    // Obtain the back buffer for this window which will be used
+    // for the final 3D render target.
+    ID3D11Texture2D* backBuffer = NULL;
+    if (FAILED(g_pSwapChain->GetBuffer(0, IID_PPV_ARGS(&backBuffer))))
+        return false;
+
+    //为后台缓冲区的RenderTargetView设置描述符。
+    CD3D11_RENDER_TARGET_VIEW_DESC renderTargetViewDesc(D3D11_RTV_DIMENSION_TEXTURE2DARRAY, DXGI_FORMAT_B8G8R8A8_UNORM, 0, 0, 1);
+
+    //在渲染目标上创建一个视图界面，用于单声道或左眼视图的绑定。
+    if (FAILED(g_pd3dDevice->CreateRenderTargetView(backBuffer, nullptr, &g_backBufferRenderTargetView)))//只能为null，否则失败
+    {
+        backBuffer->Release();
+        return false;
+    }
+
+    // 立体交换链具有阵列资源，因此创建第二个渲染目标。
+    CD3D11_RENDER_TARGET_VIEW_DESC renderTargetViewRightDesc(D3D11_RTV_DIMENSION_TEXTURE2DARRAY, DXGI_FORMAT_B8G8R8A8_UNORM, 0, 1, 1);
+    if (FAILED(g_pd3dDevice->CreateRenderTargetView(backBuffer, nullptr , &g_rightRenderTargetView)))
+    {
+        backBuffer->Release();
+        return false;
+    }
+
+    // Release the back buffer.
+    backBuffer->Release();
+
+    // Cache the render target dimensions in our helper class for convenient use 在我们的帮助器类中缓存渲染目标维度以方便使用
+    D3D11_TEXTURE2D_DESC backBufferDesc = { 0 };
+    backBuffer->GetDesc(&backBufferDesc);
+
+    // Set up the descriptor for the depth/stencil buffer. 设置深度/模板缓冲区的描述符。
+    CD3D11_TEXTURE2D_DESC depthStencilDesc(
+        DXGI_FORMAT_D24_UNORM_S8_UINT,
+        backBufferDesc.Width,
+        backBufferDesc.Height,
+        1,
+        1,
+        D3D11_BIND_DEPTH_STENCIL);
+
+    // Allocate a 2-D surface as the depth/stencil buffer. 将2-D表面分配为深度/模板缓冲区。
+    ID3D11Texture2D* depthStencil = NULL;
+    if (FAILED(g_pd3dDevice->CreateTexture2D(&depthStencilDesc, NULL, &depthStencil)))
+        return false;
+
+    // Create a depth stencil view on this surface to use on bind. 在此曲面上创建深度模板视图以在绑定时使用。
+    CD3D11_DEPTH_STENCIL_VIEW_DESC viewDesc(D3D11_DSV_DIMENSION_TEXTURE2D);
+    HRESULT isDepthStencilViewCreated = g_pd3dDevice->CreateDepthStencilView(depthStencil, &viewDesc, &g_backBufferDepthStencilView);
+    depthStencil->Release();
+
+    if (FAILED(isDepthStencilViewCreated))
+        return false;
+
+    // Create a viewport of the full window size. 创建完整窗口大小的视口。
+    CD3D11_VIEWPORT viewport(
+        0.0f,
+        0.0f,
+        static_cast<float>(backBufferDesc.Width),
+        static_cast<float>(backBufferDesc.Height));
+
+    // Set the current viewport.
+    g_pImmediateContext1->RSSetViewports(1, &viewport);
+    return true;
 }
